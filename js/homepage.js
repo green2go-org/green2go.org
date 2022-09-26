146f9b89bba7389f08f33c38e042b1952f3c7317
amandajaisun <amandajaisun@gmail.com>
2022-09-26 10:50:06 -0400

    Adding javascript to calculate dollars saved.

---
+++ js/homepage.js
@@ -0,0 +1,12 @@
+let moneyElement = document.getElementById("dollars-saved");
+
+if (moneyElement !== null) {
+    var now = new Date();
+    var start = new Date(now.getFullYear(), 0, 0);
+    var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    var oneDay = 1000 * 60 * 60 * 24;
+    var day = Math.floor(diff / oneDay);
+    console.log('Day of year: ' + day);
+    let dollarsSaved = Math.round((9000/day)* 100) / 100);
+    moneyElement.innerText = dollarsSaved;
+}
