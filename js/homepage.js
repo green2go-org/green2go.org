9e69ff9c6613958af7ffdc78140331e9a12a1061
Evan Baldonado <21182871+evanbaldonado@users.noreply.github.com>
2022-09-26 07:57:27 -0700

    Fix dollar counter algorithm

--- js/homepage.js
+++ js/homepage.js
@@ -6,7 +6,7 @@ if (moneyElement !== null) {
     var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
     var oneDay = 1000 * 60 * 60 * 24;
     var day = Math.floor(diff / oneDay);
-    console.log('Day of year: ' + day);
-    let dollarsSaved = Math.round((9000/day)* 100) / 100);
+    let totalDaysInYear = ((now.getYear() % 4 === 0 && now.getYear() % 100 > 0) || now.getYear() % 400 == 0) ? 366 : 365;
+    let dollarsSaved = Math.round((9000 / totalDaysInYear * day) * 100) / 100;
     moneyElement.innerText = dollarsSaved;
 }
